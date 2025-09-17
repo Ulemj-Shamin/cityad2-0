2c1b11b57000055927b0f48564d62eaa8fbcea72
Ulemj Shamin <ulemj@cityad.biz>
2025-09-17 16:05:02 +0800

    Fix supabaseClient import path in Signin.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './supabaseClient';
 
 export default function Login() {
   const { t } = useTranslation();
