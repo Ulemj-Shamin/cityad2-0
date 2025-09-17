52885e0af1c22be08c8c75c025454e3c7b173f35
Ulemj Shamin <ulemj@cityad.biz>
2025-09-17 17:42:25 +0800

    Fix routing: render Page for /login, /signup, /dashboard v3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,14 @@ export default function Navbar() {
 
   return (
     <nav className="text-cyan text-center py-4">
-      <Link className="mx-2 hover:text-white" to="/">{t('navbar.home')}</Link> |
-      <Link className="mx-2 hover:text-white" to="/about">{t('navbar.about')}</Link> |
-      <Link className="mx-2 hover:text-white" to="/contact">{t('navbar.contact')}</Link> |
-      <Link className="mx-2 hover:text-white" to="/faq">{t('navbar.faq')}</Link> |
-      <Link className="mx-2 hover:text-white" to="/login">{t('navbar.login')}</Link>
-      <button className="ml-4 px-2 py-1 border border-cyan rounded" onClick={()=>changeLanguage('en')}>EN</button>
-      <button className="ml-2 px-2 py-1 border border-cyan rounded" onClick={()=>changeLanguage('mn')}>MN</button>
-    </nav>
+  <Link className="mx-2 hover:text-white" to="/">{t('navbar.home')}</Link> |
+  <Link className="mx-2 hover:text-white" to="/about">{t('navbar.about')}</Link> |
+  <Link className="mx-2 hover:text-white" to="/contact">{t('navbar.contact')}</Link> |
+  <Link className="mx-2 hover:text-white" to="/faq">{t('navbar.faq')}</Link> |
+  <Link className="mx-2 hover:text-white" to="/login">{t('navbar.login')}</Link> |
+  <Link className="mx-2 hover:text-white" to="/signup">{t('navbar.signup')}</Link> {/* Add this */}
+  <button className="ml-4 px-2 py-1 border border-cyan rounded" onClick={()=>changeLanguage('en')}>EN</button>
+  <button className="ml-2 px-2 py-1 border border-cyan rounded" onClick={()=>changeLanguage('mn')}>MN</button>
+</nav>
   );
 }
